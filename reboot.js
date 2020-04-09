e26df5702b3c9462c503dc336bf4102a301cb8a9
jallend1 <jallend1@gmail.com>
2020-04-09 15:30:05 -0700

    Refactors both player and alien movement functions

--- reboot.js
+++ reboot.js
@@ -1,139 +1,110 @@
 const gameArea = document.querySelector('#gamearea');
-const resultDisplay = document.querySelector('#score');
-const gameDimensions = 15;
-
-let width = 15;
-let currentShooterIndex = 202;
-let currentInvaderIndex = 0;
-let alienInvadersTakenDown = [];
-let result = 0;
+const gameSize = 15;
+const cells = generateBoard();
+let playerPosition = 202;
 let direction = 1;
-let invaderId;
+const aliens = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+                15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+                30, 31, 32, 33, 34, 35, 36, 37, 38, 39];
+const lasers = [];
 
-const alienInvaders = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39 
-];
 
-function checkGameOver(){
-    const cells = document.querySelectorAll('.cell');
-    if(cells[currentShooterIndex].classList.contains('invader', 'shooter')){
-        resultDisplay.textContent = 'Game Over';
-        cells[currentShooterIndex].classList.add('boom');
-        clearInterval(invaderId);
-    }
-    for(let i = 0; i <= alienInvaders.length - 1; i++){
-        if(alienInvaders[i] > cells.length - (width - 1)){
-            resultDisplay.textContent = 'Game Over';
-            clearInterval(invaderId);
+
+function gameOver(){
+    cells.forEach(cell => {
+        if(cell.classList.contains('player') && cell.classList.contains('alien')){              // Ends game if player and aliens collide
+            console.log('LOSER');
+            clearInterval(gameProgress);
         }
-    }
-    if(alienInvadersTakenDown.length === alienInvaders.length){
-        resultDisplay.textContent = 'You Win!'
-        clearInterval(invaderId);
-    }
-}
-
-function createEnterprise(){
-    const cells = document.querySelectorAll('.cell');
-    cells[currentShooterIndex].classList.add('shooter');
-}
-
-function createRomulans(){
-    const cells = document.querySelectorAll('.cell');
-    alienInvaders.forEach(invader => cells[invader + currentInvaderIndex].classList.add('invader'));
-
-}
-
-function firePhasers(e){
-    let phaserId;
-    let currentLaserIndex = currentShooterIndex;
-    
-    function movePhasers(){
-        const cells = document.querySelectorAll('.cell');
-        cells[currentLaserIndex].classList.remove('laser');
-        currentLaserIndex -= width;
-        cells[currentLaserIndex].classList.add('laser');
-        if(cells[currentLaserIndex].classlist.contains('invader')){
-            cells[currentLaserIndex].classList.remove('laser');
-            cells[currentLaserIndex].classList.remove('invader');
-            cells[currentLaserIndex].classList.add('boom');
-
-            setTimeout(() => cells[currentLaserIndex].classList.remove('boom'), 250)
-            clearInterval(phaserId);
-
-            const alienTakenDown = alienInvaders.indexOf(currentLaserIndex);
-            alienInvadersTakenDown.push(alienTakenDown);
-            result++;
-            resultDisplay.textContent = result;
+    })
+    aliens.forEach(alien => {
+        if(alien > (gameSize * gameSize) - (gameSize * 2)){
+            console.log('LOSER AGAIN');
+            clearInterval(gameProgress);
         }
-        if(currentLaserIndex < width){
-            clearInterval(phaserId);
-            setTimeout(() => cells[currentLaserIndex].classList.remove('laser'), 100);
-        }    
-    }
-        if(e.keyCode === 32){
-            phaserId = setInterval(movePhasers, 100);
-        }
+    })
 }
-
-window.addEventListener('keyup', firePhasers)
-
 
 function generateBoard(){
-    for(let i = 0; i < (gameDimensions * gameDimensions); i++){
-        const newCell = document.createElement('div');
-        newCell.classList.add('cell');
-        gameArea.appendChild(newCell);
+    for(let i = 0; i < gameSize * gameSize; i++){
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('cell');
+        gameArea.appendChild(newDiv);
     }
+    return document.querySelectorAll('.cell');
 }
 
-function moveEnterprise(e){
-    const cells = document.querySelectorAll('.cell');
-    cells[currentShooterIndex].classList.remove('shooter');
-    if(e.keyCode === 37){
-        if(currentShooterIndex % width !== 0){
-            currentShooterIndex -= 1;
+function generateAliens(){
+    aliens.forEach(alien => {
+        cells[alien].classList.add('alien')
+    });
+}
+
+function init(){
+    cells[playerPosition].classList.add('player');
+    generateAliens();
+}
+
+function moveAliens(){
+    aliens.forEach(alien => cells[alien].classList.remove('alien'))     // Clears alien classlist
+    if((aliens[aliens.length - 1] + 1) % gameSize !== 0 && direction === 1){    //Advance aliens if they aren't on the right edge
+        for(let i = 0; i < aliens.length; i++){
+            aliens[i] += direction;
         }
     }
-    else if(e.keyCode === 39){
-        if(currentShooterIndex % width < width - 1){
-                currentShooterIndex += 1;
-            }
-    }
-    cells[currentShooterIndex].classList.add('shooter');
-}
-
-function moveRomulans(){
-    const cells = document.querySelectorAll('.cell');
-    const leftEdge = alienInvaders[0] % width === 0;
-    const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width - 1;
-    
-    if((leftEdge && direction === -1) || (rightEdge && direction === 1)){
-        direction = width;
-    } else if(direction === width){
-        leftEdge ? direction = 1 : direction = -1;
-    }
-    for(let i = 0; i <= alienInvaders.length - 1; i++){                 // Clears existing Romulans
-        cells[alienInvaders[i]].classList.remove('invader');
-    }
-    for(let i = 0; i <= alienInvaders.length - 1; i++){                 // Adds Romulans to new location
-        alienInvaders[i] += direction;
-        if(!alienInvadersTakenDown.includes(i)){
-            cells[alienInvaders[i]].classList.add('invader');
+    else if((aliens[0] % gameSize !== 0 && direction === -1)){                    // Move aliens left if they aren't on edge
+        for(let i = 0; i < aliens.length; i++){
+            aliens[i] += direction;
         }
+    }else{
+        for(let i = 0; i < aliens.length; i++){
+            aliens[i] += gameSize;
+        }
+        direction = direction * -1;
+        
     }
-    checkGameOver();
+    generateAliens();
 }
 
+function movePlayer(e){
+    if(e.keyCode === 37 && playerPosition % gameSize !== 0){            // If left arrow pressed and not on left edge, move left
+            cells[playerPosition].classList.remove('player');               // Removes styling for previous square
+            playerPosition--;
+            cells[playerPosition].classList.add('player');                  // Adds player styling to current square
+    }
+    else if(e.keyCode === 39 && (playerPosition + 1) % gameSize !== 0){
+        cells[playerPosition].classList.remove('player');
+        playerPosition++;
+        cells[playerPosition].classList.add('player');
+    }
+}
 
+function update(){
+    moveAliens();
+    gameOver();
+}
 
+function updateLasers(){
+    for(let i = 0; i < lasers.length; i++){
+        cells[lasers[i]].classList.remove('laser');
+        lasers[i] -= gameSize;
+    }
+    lasers.forEach((laser) => {
+            cells[laser].classList.add('laser');
+            cells[laser-gameSize].classList.remove('laser');
+        })
+}
 
+init();
 
-generateBoard();
-createRomulans();
-createEnterprise();
+window.addEventListener('keydown', e => {
+    if(e.keyCode === 37 || e.keyCode === 39){
+        movePlayer(e);
+    }
+    else if(e.keyCode === 32){
+        lasers.push(playerPosition);
+        updateLasers();
+    }
+});
 
-window.addEventListener('keydown', moveEnterprise);
-invaderId = setInterval(moveRomulans, 500);
+const gameProgress = setInterval(update, 500);
