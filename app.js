3e16bbad30e7847694369ad6c63a7d77dca0cdf6
jallend1 <jallend1@gmail.com>
2020-05-02 14:59:20 -0700

    Completes alien movement functionality and establishes game over conditions

--- app.js
+++ app.js
@@ -3,10 +3,33 @@ const gameSize = 15;
 let playerPosition = Math.floor((gameSize * gameSize) - (gameSize / 2));
 let playerDirection = 0;
 let alienDirection = 1;
+let moveRight = true;
 const aliens = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
                 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
                 30, 31, 32, 33, 34, 35, 36, 37, 38, 39];
                 
+function checkAlienBoundaries(){
+    if(alienDirection === 0){                                           // If this is the first time on this row, change the direction value
+        moveRight ? alienDirection = 1 : alienDirection = -1;
+        moveRight = !moveRight;
+    }
+    else if(aliens.some(alien => (alien + 1) % gameSize === 0)){        // If the next turn moves them to a different row
+        moveRight = false;
+        alienDirection = 0;                                             // Transitory status of 0, so aliens ONLY move down this cycle
+        for(let i = 0; i < aliens.length; i++){                         // Move ALL aliens down a row
+            aliens[i] += gameSize;
+        }
+    }
+    else if(aliens.some(alien => alien % gameSize === 0)                 // If any of the aliens on are on the left edge
+        && aliens.every(alien => alien > 0)){                           // AND it isn't the starting frame
+            alienDirection = 0;                                         // Transitory status of 0, so aliens ONLY move down this cycle
+            moveRight = true;
+            for(let i = 0; i < aliens.length; i++){                     // Move all aliens down a row
+                aliens[i] += gameSize;
+            }
+        }
+}
+
 function checkPlayerBoundaries(){                                     // Keeps player on last row
     if(playerPosition > (gameSize * gameSize) - 1){
         playerPosition = (gameSize * gameSize) - 1;
@@ -16,20 +39,9 @@ function checkPlayerBoundaries(){                                     // Keeps p
     }
 }
 
-function determineDirection(){
-    console.log('hello');
-    // if(aliens.some(alien => (alien + 1) % gameSize === 0)){
-    //     alienDirection = -1;
-    //     for(let i = 0; i < aliens.length; i++){
-    //         aliens[i] += gameSize;
-    //     }
-    // }
-    // else if(aliens.some(alien => alien % gameSize === 0)){
-    //     alienDirection = 1;
-    //     for(let i = 0; i < aliens.length; i++){
-    //         aliens[i] += gameSize;
-    //     }
-    // }
+function gameOver(){
+    console.log('Game over man');
+    clearInterval(eachTurn);
 }
 
 function generateBoard(){                                       // Generates divs inside of game area
@@ -44,17 +56,11 @@ function generateBoard(){                                       // Generates div
 function positionAliens(){
     const gameBoard = document.querySelectorAll('.cell');
     gameBoard.forEach(cell => cell.classList.remove('alien'));          // Removes previous alien locations
-    if(aliens.some(alien => alien % (gameSize - 1) === 0)){
-        for(let i = 0; i < aliens.length; i++){
-            aliens[i] += gameSize;
-        }
-    }else{
-        for(let i = 0; i < aliens.length; i++){
-            aliens[i] += alienDirection;
-        }
+    checkAlienBoundaries();
+    for(let i = 0; i < aliens.length; i++){                             // Moves all aliens the desired direction
+        aliens[i] += alienDirection;
     }
     aliens.forEach(alien => gameBoard[alien].classList.add('alien'));
-    // determineDirection();                                               // Makes sure aliens are inside game area
 }
 
 function positionPlayer(){
@@ -67,9 +73,11 @@ function positionPlayer(){
 }
 
 function gameTurn(){
-    positionPlayer()
+    positionPlayer();
     positionAliens();
-    determineDirection();
+    if(aliens.some(alien => alien >= gameSize * gameSize - gameSize)){
+        gameOver();
+    }
 }
 
 function init(){
@@ -82,8 +90,6 @@ function init(){
 init();
 
 const eachTurn = setInterval(gameTurn, 500);
-
-
 
 window.addEventListener('keydown', e => {
     if(e.keyCode === 37){
